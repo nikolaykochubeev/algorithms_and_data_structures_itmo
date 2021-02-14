37ff799bd811d833aee27f0ec783bf077eafe7ae
nikolai <nikolay.kochubeev@gmail.com>
2021-02-14 23:03:36 +0300

    Done

---
+++ 8 laboratory/8c.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+    int n;
+    cin >> n;
+    vector <vector<int>> adjacency_matrix(n);
+    for (auto &string: adjacency_matrix){
+        string.resize(n);
+        for (auto &element: string){
+            cin >> element;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if ((adjacency_matrix[i][j]) and (adjacency_matrix[i][j] == adjacency_matrix[j][i])){
+                cout << "YES";
+                return 0;
+            }
+        }
+    }
+    cout << "NO";
+    return 0;
+}
